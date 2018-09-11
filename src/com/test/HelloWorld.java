da8820470f3b876dd590d29728f58e3dd0393f64
pop8682 <bgpark82@gmail.com>
2018-09-11 09:55:59 +0900

    first branch test

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld {
 		
 		System.out.println("GOD DAMN HELLO WORLD!!!");
 		System.out.println("Do I receive this?");
+		System.err.println("It seems work fine");
+		System.out.println("This is the first branch test");
+		
 	}
 }
