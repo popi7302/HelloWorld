c1022182de104527184c113e0a10716fc9d055dd
popi7302 <travelbgpark@gmail.com>
2018-09-11 10:17:12 +0900

    pulling issue perhaps it caused by remote tracking

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("GOD DAMN HELLO FUCKING WORLD!!!");
+		System.out.println("something wrong with pulling");
 	}
 }
