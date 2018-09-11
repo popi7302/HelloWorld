2e1194d653c39353c20696be752fe6edf885422f
pop8682 <bgpark82@gmail.com>
2018-09-11 09:11:58 +0900

    initial change

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -2,6 +2,8 @@ package com.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("GOD DAMN HELLO FUCKING WORLD!!!");
+		
+		System.out.println("GOD DAMN HELLO WORLD!!!");
+		System.out.println("Do I receive this?");
 	}
 }
